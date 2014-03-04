b0f3b08a8f2ad8a0c2ef2cb2bf998d1372f8b3df
Deepak Srivastava <deepak.srivastava.0303@gmail.com>
2014-03-04 22:20:55 +0530

    use group by for count correctness

--- CRM/Contribute/BAO/Query.php
+++ CRM/Contribute/BAO/Query.php
@@ -355,10 +355,12 @@ class CRM_Contribute_BAO_Query {
         if ($value == 'only_scredits') {
           $query->_where[$grouping][] = "contribution_search_scredit_combined.scredit_id IS NOT NULL";
           $query->_qill[$grouping][] = ts('Contributions OR Soft Credits? - Soft Credits Only');
+          $query->_tables['civicrm_contribution'] = $query->_whereTables['civicrm_contribution'] = 1;
           $query->_tables['civicrm_contribution_soft'] = $query->_whereTables['civicrm_contribution_soft'] = 1;
         } else if ($value == 'both_related') {
           $query->_where[$grouping][] = "contribution_search_scredit_combined.filter_id IS NOT NULL";
           $query->_qill[$grouping][] = ts('Contributions OR Soft Credits? - Both But Related');
+          $query->_tables['civicrm_contribution'] = $query->_whereTables['civicrm_contribution'] = 1;
           $query->_tables['civicrm_contribution_soft'] = $query->_whereTables['civicrm_contribution_soft'] = 1;
         } else if ($value == 'both') {
           $query->_qill[$grouping][] = ts('Contributions OR Soft Credits? - Both');
